aa70439e8fc99aa137cf5a6b77555fd13c743d48
Ліпов Богдан Богданович <98353650+LipovBogdanPV@users.noreply.github.com>
2025-06-22 17:02:55 +0300

    Delete orders directory

--- orders/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),  # головна
-    path('create/', views.create_order, name='create_order'),
-    path('success/', views.order_success, name='order_success'),
-]
