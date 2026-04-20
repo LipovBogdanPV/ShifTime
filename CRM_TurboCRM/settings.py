e519aec3a4ec31b2ee6e69d1473923463253365a
Ліпов Богдан Богданович <98353650+LipovBogdanPV@users.noreply.github.com>
2026-04-20 20:46:35 +0300

    Update settings.py

--- CRM_TurboCRM/settings.py
+++ CRM_TurboCRM/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--4&-divn=+%@8wa-!8v2h-b-ics)uy-!sjn!h_i1#@lobvwxg^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'shiftime-1.onrender.com']
 
 
 # Application definition
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shopCRM',
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -74,10 +74,7 @@ WSGI_APPLICATION = 'CRM_TurboCRM.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
 }
 
 
