060d1743de46d742178626dfd514cbec5e54e288
Ліпов Богдан Богданович <98353650+LipovBogdanPV@users.noreply.github.com>
2026-04-20 20:03:25 +0300

    Delete shifttime directory

--- shifttime/crm/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Order
-
-class OrderForm(forms.ModelForm):
-    class Meta:
-        model = Order
-        fields = ["full_name", "phone", "address", "post_office", "comment"]
--- shifttime/crm/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("success/", views.success, name="success"),
-    path("Add_product/", views.add_product, name="add_product"),
-    path("Order_list/", views.order_list, name="order_list"),
-]
